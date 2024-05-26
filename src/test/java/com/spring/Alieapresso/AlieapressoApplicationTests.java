4ed3e75d3c1353ad16bf39a5cb867460bc155d2e
A1BeRtO5 <maks000500@gmail.com>
2024-05-26 22:36:06 +0300

    add 1task

---
+++ src/test/java/com/spring/Alieapresso/AlieapressoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.Alieapresso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlieapressoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
