f3c46ebb891aa87a17047dbebc397c9214b107c2
A1BeRtO5 <maks000500@gmail.com>
2024-06-02 23:09:10 +0300

    problem css

--- src/main/java/com/spring/Alieapresso/controllers/Controller.java
+++ src/main/java/com/spring/Alieapresso/controllers/Controller.java
@@ -1,28 +1,28 @@
-package com.spring.Alieapresso.controllers;
-
-import org.springframework.web.bind.annotation.GetMapping;
-
-@org.springframework.stereotype.Controller
-public class Controller {
-    @GetMapping("/page1.html")
-    public String tovari(){
-        return "page1";
-    }
-    @GetMapping("/page2.html")
-    public String tovari1(){
-        return "page2";
-    }
-    @GetMapping("/")
-    public String home(){
-        return "home";
-    }
-    @GetMapping("/index.html")
-    public String index(){
-        return "index.html";
-    }
-    @GetMapping("/5")
-    public String lalala(){
-        return "test";
-    }
-
-}
+//package com.spring.Alieapresso.controllers;
+//
+//import org.springframework.web.bind.annotation.GetMapping;
+//
+//@org.springframework.stereotype.Controller
+//public class Controller {
+//    @GetMapping("/page1.html")
+//    public String tovari(){
+//        return "/html/page1";
+//    }
+//    @GetMapping("/page2.html")
+//    public String tovari1(){
+//        return "/html/page2";
+//    }
+//    @GetMapping("/")
+//    public String home(){
+//        return "/html/home";
+//    }
+//    @GetMapping("/index.html")
+//    public String index(){
+//        return "/html/index.html";
+//    }
+//    @GetMapping("/5")
+//    public String lalala(){
+//        return "/html/test.html";
+//    }
+//
+//}
